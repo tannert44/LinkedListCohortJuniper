f7e433d35e2bc8ff8b058105dad41b7de70011e8
tannert44 <tannert8155@gmail.com>
2015-10-29 10:33:30 -0500

    Safety Committ

--- SinglyLinkedLists/SinglyLinkedList.cs
+++ SinglyLinkedLists/SinglyLinkedList.cs
@@ -7,17 +7,16 @@ namespace SinglyLinkedLists
 {
     public class SinglyLinkedList
     {
-        public SinglyLinkedList()
-        {
-            // NOTE: This constructor isn't necessary, once you've implemented the constructor below.
-        }
-
+        private SinglyLinkedListNode first;
+       
+        //List Constructor: Needs a first node. 
         // READ: http://msdn.microsoft.com/en-us/library/aa691335(v=vs.71).aspx
         public SinglyLinkedList(params object[] values)
         {
-            throw new NotImplementedException();
+            first = new SinglyLinkedListNode(null);
         }
 
+        //UNKNOWN TERRITORY
         // READ: http://msdn.microsoft.com/en-us/library/6x16t2tx.aspx
         public string this[int i]
         {
@@ -25,68 +24,159 @@ namespace SinglyLinkedLists
             set { throw new NotImplementedException(); }
         }
 
+        // Adds A Node Value after Specified Existing Node Value in the SLL
         public void AddAfter(string existingValue, string value)
         {
             throw new NotImplementedException();
         }
-
+        //Adds A Node to the beginning of to SLL
         public void AddFirst(string value)
         {
-            throw new NotImplementedException();
+            SinglyLinkedListNode  node = first;
+            if (node.Value == null)
+            {
+                first = new SinglyLinkedListNode(value);
+            }
+            else
+            {
+               
+            }
         }
-
+        //Adds a Node Value to the end of the SLL
         public void AddLast(string value)
         {
-            throw new NotImplementedException();
+            SinglyLinkedListNode node = first;
+            if (first.Value == null)
+            {
+                first = new SinglyLinkedListNode(value);
+            }
+            while (!node.IsLast())
+            {
+                node = node.Next;
+            }
+            node.Next = new SinglyLinkedListNode(value);
         }
 
-        // NOTE: There is more than one way to accomplish this.  One is O(n).  The other is O(1).
+        // NOTE: There is more than one way to accomplish this.  One is O(n).  The other is O(1). Counts the Node Values in SLL
         public int Count()
         {
             throw new NotImplementedException();
         }
-
+        //Finds an element at index based on UNKNOW TERRITORY
         public string ElementAt(int index)
         {
-            throw new NotImplementedException();
+            if (this.First() == null)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            SinglyLinkedListNode node = first;
+            for (int i = 0; i <= index; i++)
+            {
+                if (i == index)
+                {
+                    break;
+                }
+                else
+                {
+                    if (node.Next == null)
+                    {
+                        throw new ArgumentOutOfRangeException();
+                    }
+                    node = node.Next;
+                }
+            }
+            return node.Value;
         }
-
+        //Returns First Node Value in SLL
         public string First()
         {
-            throw new NotImplementedException();
+            return first.Value;
         }
-
+        //Finds the index of a Specifice Node Value in SLL
         public int IndexOf(string value)
         {
             throw new NotImplementedException();
         }
-
+        //Determines if SLL is in Alphabetical order
         public bool IsSorted()
         {
             throw new NotImplementedException();
         }
-
+        //Returns the last Node Follow Hints
         // HINT 1: You can extract this functionality (finding the last item in the list) from a method you've already written!
         // HINT 2: I suggest writing a private helper method LastNode()
         // HINT 3: If you highlight code and right click, you can use the refactor menu to extract a method for you...
         public string Last()
         {
-            throw new NotImplementedException();
+            SinglyLinkedListNode node = first;
+            if (node.Value == null)
+            {
+                return null;
+            }
+            else
+            {
+                while (!node.IsLast())
+                {
+                    node = node.Next;
+                }
+                return node.Value;
+            }
         }
-
+        //Removes Specified Node Value in SLL
         public void Remove(string value)
         {
             throw new NotImplementedException();
         }
-
+        //Puts Items in alphabetical or numerical order
         public void Sort()
         {
             throw new NotImplementedException();
         }
-
+        //Changes SLL into a fixed sized array
         public string[] ToArray()
         {
-            throw new NotImplementedException();
+            List<string> results = new List<string>();
+            SinglyLinkedListNode node = first;
+            if (First() == null)
+            {
+                return new string[] { };
+            }
+            else
+            {
+                while (node != null)
+                {
+                    results.Add(node.Value);
+                    node = node.Next;
+                }
+                return results.ToArray();
+            }
+        }
+
+        public override string ToString()
+        {
+            if (first.Value == null)
+            {
+                return "{ }";
+            }
+            else
+            {
+                SinglyLinkedListNode node = first;
+                string result = "{ ";
+                while (node != null)
+                {
+                    if (node.Next == null)
+                    {
+                        result += "\"" + node.Value + "\" ";
+                    }
+                    else
+                    {
+                        result += "\"" + node.Value + "\", ";
+                    }
+                    node = node.Next;
+                }
+                result += "}";
+                return result;
+            }
         }
     }
 }
--- SinglyLinkedLists/SinglyLinkedListNode.cs
+++ SinglyLinkedLists/SinglyLinkedListNode.cs
@@ -10,21 +10,28 @@ namespace SinglyLinkedLists
     {
         // Used by the visualizer.  Do not change.
         public static List<SinglyLinkedListNode> allNodes = new List<SinglyLinkedListNode>();
-
+        //Getter and Setter for next on node in SLL
         // READ: http://msdn.microsoft.com/en-us/library/aa287786(v=vs.71).aspx
         private SinglyLinkedListNode next;
         public SinglyLinkedListNode Next
         {
-            get { throw new NotImplementedException(); }
-            set { throw new NotImplementedException(); }
+            get { return next; }
+            set
+            {
+                if (this == value)
+                {
+                    throw new ArgumentException();
+                }
+                this.next = value;
+            }
         }
-
+        //Gets the value on node in SLL
         private string value;
         public string Value 
         {
-            get { throw new NotImplementedException(); }
+            get { return value; }
         }
-
+        
         public static bool operator <(SinglyLinkedListNode node1, SinglyLinkedListNode node2)
         {
             // This implementation is provided for your convenience.
@@ -36,24 +43,48 @@ namespace SinglyLinkedLists
             // This implementation is provided for your convenience.
             return node1.CompareTo(node2) > 0;
         }
-
+        //Constructor for A Node Needs value and next Intializes node
         public SinglyLinkedListNode(string value)
         {
-            throw new NotImplementedException();
+            this.value = value;
 
             // Used by the visualizer:
             allNodes.Add(this);
         }
-
+        //determines which node shows up first
         // READ: http://msdn.microsoft.com/en-us/library/system.icomparable.compareto.aspx
         public int CompareTo(Object obj)
         {
-            throw new NotImplementedException();
-        }
+            if (obj == null) return 1;
 
+            SinglyLinkedListNode otherNode = obj as SinglyLinkedListNode;
+            if (otherNode != null)
+                return this.Value.CompareTo(otherNode.Value);
+            else
+                throw new ArgumentException("Object Value was null");
+        }
+        //Returns if Node Value is the Last Value in SLL
         public bool IsLast()
         {
-            throw new NotImplementedException();
+            return (Next == null);
+        }
+
+        public override string ToString()
+        {
+            return Value;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is SinglyLinkedListNode)
+            {
+                SinglyLinkedListNode node = obj as SinglyLinkedListNode;
+                return Equals(this.Value, node.Value);
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
